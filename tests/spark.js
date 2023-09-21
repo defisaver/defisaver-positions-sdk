7a7889a00b4fd9486b5caa476d9575c06f166f71
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2023-09-21 14:35:31 +0200

    Add getFullPositionData to spark

--- tests/spark.js
+++ tests/spark.js
@@ -33,11 +33,26 @@ describe('Spark', () => {
     ]);
   };
 
+  const fetchFullPositionData = async (network, _web3) => {
+    const positionData = await sdk.aaveV3.getAaveV3FullPositionData(_web3, network, '0x9cCf93089cb14F94BAeB8822F8CeFfd91Bd71649', sdk.markets.SparkMarkets(network)[sdk.SparkVersions.SparkV1], web3);
+    // console.log(positionData);
+    assert.containsAllKeys(positionData, [
+      'usedAssets', 'suppliedUsd', 'borrowedUsd', 'ratio', 'eModeCategories', // ...
+    ]);
+  };
+
   it('can fetch market and account data for Ethereum', async function () {
     this.timeout(10000);
     const network = NetworkNumber.Eth;
 
     const marketData = await fetchMarketData(network, web3);
     await fetchAccountData(network, web3, marketData);
+  });
+
+  it('can fetch full position data for Ethereum', async function () {
+    this.timeout(10000);
+    const network = NetworkNumber.Eth;
+
+    await fetchFullPositionData(network, web3);
   });
 });
