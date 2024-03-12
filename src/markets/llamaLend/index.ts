55d2b883ed017073a745241f001541b61668776a
sterlu <sterlu.sd@gmail.com>
2024-03-12 13:10:49 +0100

    Version bump to 0.0.50

--- src/markets/llamaLend/index.ts
+++ src/markets/llamaLend/index.ts
@@ -9,7 +9,7 @@ export const LLAMALEND_WSTETH_CRVUSD_MARKET = (networkId: NetworkNumber): LlamaL
   value: LlamaLendVersions.LlamaLendwstETHcrvUSD,
   collAsset: 'wstETH',
   baseAsset: 'crvUSD',
-  controllerAddress: '0x5E657c5227A596a860621C5551c9735d8f4A8BE3',
+  controllerAddress: '0x179Fc527ec9Dc8c041F7ce486C8667F23373724A',
   vaultAddress: '0xE21C518a09b26Bf65B16767B97249385f12780d9',
   url: 'wstethcrvusd',
 });
@@ -20,7 +20,7 @@ export const LLAMALEND_CRVUSD_CRV_MARKET = (networkId: NetworkNumber): LlamaLend
   value: LlamaLendVersions.LlamaLendcrvUSDCRV,
   collAsset: 'crvUSD',
   baseAsset: 'CRV',
-  controllerAddress: '0x43fc0f246F952ff12B757341A91cF4040711dDE9',
+  controllerAddress: '0x02C4f19F69d85d215b6eb714e01EF8728Bb57d56',
   vaultAddress: '0x044aC5160e5A04E09EBAE06D786fc151F2BA5ceD',
   url: 'crvusdcrv',
 });
@@ -31,7 +31,7 @@ export const LLAMALEND_CRV_CRVUSD_MARKET = (networkId: NetworkNumber): LlamaLend
   value: LlamaLendVersions.LlamaLendCRVcrvUSD,
   collAsset: 'CRV',
   baseAsset: 'crvUSD',
-  controllerAddress: '0x7443944962D04720f8c220C0D25f56F869d6EfD4',
+  controllerAddress: '0xa8A095743cA0D283139e2FB62F5e63812D6d7B24',
   vaultAddress: '0x67A18c18709C09D48000B321c6E1cb09F7181211',
   url: 'crvcrvusd',
 });
