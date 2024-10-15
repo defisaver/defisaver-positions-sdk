53cbc2b106d54c78ec0066a8be1639ba2ca7e7f4
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2024-10-15 14:26:28 +0200

    Refactor liquity to ts

--- tests/liquity.ts
+++ tests/liquity.ts
@@ -1,18 +1,23 @@
-require('dotenv').config();
-const { assert } = require('chai');
-const Web3 = require('web3');
+import 'dotenv/config';
+import Web3 from 'web3';
 
-const sdk = require('../src');
-const { NetworkNumber } = require('../src/types/common');
+import * as sdk from '../src';
+
+import { Blockish, NetworkNumber } from '../src/types/common';
+
+const { assert } = require('chai');
 
 describe('Liquity', () => {
-  let web3;
+  let web3: Web3;
   before(async () => {
+    if (!process.env.RPC) {
+      throw new Error('RPC environment variable is not defined.');
+    }
     web3 = new Web3(process.env.RPC);
   });
 
-  const fetchAccountBalances = async (network, web3, blockNumber) => {
-    const balances = await sdk.liquity.getLiquityAccountBalances(web3, network, blockNumber, false, '0x9cCf93089cb14F94BAeB8822F8CeFfd91Bd71649');
+  const fetchAccountBalances = async (network: NetworkNumber, _web3: Web3, blockNumber: Blockish) => {
+    const balances = await sdk.liquity.getLiquityAccountBalances(_web3, network, blockNumber, false, '0x9cCf93089cb14F94BAeB8822F8CeFfd91Bd71649');
     // console.log(balances);
     assert.containsAllKeys(balances, [
       'collateral', 'debt',
