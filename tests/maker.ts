86187f80d1c277ebf9b1d82c52de32abdc058957
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2024-10-15 14:35:04 +0200

    Refactor maker to ts

--- tests/maker.ts
+++ tests/maker.ts
@@ -1,18 +1,23 @@
-require('dotenv').config();
-const { assert } = require('chai');
-const Web3 = require('web3');
+import 'dotenv/config';
+import Web3 from 'web3';
 
-const sdk = require('../src');
-const { NetworkNumber } = require('../src/types/common');
+import * as sdk from '../src';
+
+import { Blockish, NetworkNumber } from '../src/types/common';
+
+const { assert } = require('chai');
 
 describe('Maker', () => {
-  let web3;
+  let web3: Web3;
   before(async () => {
+    if (!process.env.RPC) {
+      throw new Error('RPC environment variable is not defined.');
+    }
     web3 = new Web3(process.env.RPC);
   });
 
-  const fetchAccountBalances = async (network, web3, blockNumber) => {
-    const balances = await sdk.maker.getMakerAccountBalances(web3, network, blockNumber, false, '30126');
+  const fetchAccountBalances = async (network: NetworkNumber, _web3: Web3, blockNumber: Blockish) => {
+    const balances = await sdk.maker.getMakerAccountBalances(_web3, network, blockNumber, false, '30126');
     // console.log(balances);
     assert.containsAllKeys(balances, [
       'collateral', 'debt',
