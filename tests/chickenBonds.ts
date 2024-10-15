e9dc103faa381c8b1052aa8e0b215221db1b311d
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2024-10-15 13:38:03 +0200

    Refactor chickenBonds to ts

--- tests/chickenBonds.ts
+++ tests/chickenBonds.ts
@@ -1,18 +1,23 @@
-require('dotenv').config();
-const { assert } = require('chai');
-const Web3 = require('web3');
+import 'dotenv/config';
+import Web3 from 'web3';
 
-const sdk = require('../src');
-const { NetworkNumber } = require('../src/types/common');
+import * as sdk from '../src';
+
+import { Blockish, NetworkNumber } from '../src/types/common';
+
+const { assert } = require('chai');
 
 describe('ChickenBonds', () => {
-  let web3;
+  let web3: Web3;
   before(async () => {
+    if (!process.env.RPC) {
+      throw new Error('RPC environment variable is not defined.');
+    }
     web3 = new Web3(process.env.RPC);
   });
 
-  const fetchAccountBalances = async (network, web3, blockNumber) => {
-    const balances = await sdk.chickenBonds.getChickenBondsAccountBalances(web3, network, blockNumber, false, '639');
+  const fetchAccountBalances = async (network: NetworkNumber, _web3: Web3, blockNumber: Blockish) => {
+    const balances = await sdk.chickenBonds.getChickenBondsAccountBalances(_web3, network, blockNumber, false, '639');
     // console.log(balances);
     assert.containsAllKeys(balances, [
       'deposited',
@@ -53,7 +58,7 @@ describe('ChickenBonds', () => {
     this.timeout(10000);
     const network = NetworkNumber.Eth;
 
-    const bond = await sdk.chickenBonds.fetchCBondForId(web3, network, 2552);
+    const bond = await sdk.chickenBonds.fetchCBondForId(web3, network, '2552');
     // console.log(bond);
   });
 });
