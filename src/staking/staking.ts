ac28caa961620c77dc1dc9d4c6460d0fabf60609
Aleksandar Milosavljević <alemilosav@gmail.com>
2024-10-17 15:45:56 +0200

    adding staking apy info

--- src/staking/staking.ts
+++ src/staking/staking.ts
@@ -77,15 +77,36 @@ export const getDsrApy = async (web3: Web3, blockNumber: 'latest' | number = 'la
     .toString();
 };
 
+const getSuperOETHApy = async () => {
+  console.log('getSuperOETHApy');
+  const res = await fetch('https://origin.squids.live/origin-squid/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: '\n    query OTokenApy($chainId: Int!, $token: String!) {\n  oTokenApies(\n    limit: 1\n    orderBy: timestamp_DESC\n    where: {chainId_eq: $chainId, otoken_containsInsensitive: $token}\n  ) {\n    apy7DayAvg\n    apy14DayAvg\n    apy30DayAvg\n    apr\n    apy\n  }\n}\n    ',
+      variables: {
+        token: '0xdbfefd2e8460a6ee4955a68582f85708baea60a3',
+        chainId: 8453,
+      },
+    }),
+  });
+
+  const data = await res.json();
+  return data.data.oTokenApies[0].apy.toString();
+};
+
 const getApyFromDfsApi = async (asset: string) => {
   const res = await fetch(`https://app.defisaver.com/api/staking/apy?asset=${asset}`);
   const data = await res.json();
   return data.apy;
 };
 
-export const STAKING_ASSETS = ['cbETH', 'wstETH', 'cbETH', 'rETH', 'sDAI', 'weETH', 'sUSDe', 'osETH', 'ezETH', 'ETHx', 'rsETH', 'pufETH', 'wrsETH'];
+export const STAKING_ASSETS = ['cbETH', 'wstETH', 'cbETH', 'rETH', 'sDAI', 'weETH', 'sUSDe', 'osETH', 'ezETH', 'ETHx', 'rsETH', 'pufETH', 'wrsETH', 'wsuperOETHb'];
 
 export const getStakingApy = (asset: string, web3: Web3, blockNumber: 'latest' | number = 'latest', fromBlock: number | undefined = undefined) => {
+  console.log('getStakingApy', asset, blockNumber, fromBlock);
   try {
     if (asset === 'stETH' || asset === 'wstETH') return getStETHApr(web3, fromBlock, blockNumber);
     if (asset === 'cbETH') return getCbETHApr(web3, blockNumber);
@@ -98,6 +119,7 @@ export const getStakingApy = (asset: string, web3: Web3, blockNumber: 'latest' |
     if (asset === 'ETHx') return getApyFromDfsApi('ETHx');
     if (asset === 'rsETH' || asset === 'wrsETH') return getApyFromDfsApi('rsETH');
     if (asset === 'pufETH') return getApyFromDfsApi('pufETH');
+    if (asset === 'wsuperOETHb') return getSuperOETHApy();
   } catch (e) {
     console.error(`Failed to fetch APY for ${asset}`);
     return '0';
@@ -117,7 +139,7 @@ export const calculateInterestEarned = (principal: string, interest: string, typ
     return (+principal * (1 + (+interest / 100 * interval))) - +principal;
   }
 
-    return (+principal * (((1 + (+interest / 100) / BLOCKS_IN_A_YEAR)) ** (BLOCKS_IN_A_YEAR * interval))) - +principal; // eslint-disable-line
+  return (+principal * (((1 + (+interest / 100) / BLOCKS_IN_A_YEAR)) ** (BLOCKS_IN_A_YEAR * interval))) - +principal; // eslint-disable-line
 };
 
 export const calculateNetApy = (usedAssets: MMUsedAssets, assetsData: MMAssetsData, isMorpho = false) => {
