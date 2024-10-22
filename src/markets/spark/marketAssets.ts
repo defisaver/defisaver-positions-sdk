886c4b625684cb5bd2779cda746582cb7aa07044
sterlu <sterlu@users.noreply.github.com>
2024-10-22 08:12:49 +0000

    [create-pull-request] automated change

--- src/markets/spark/marketAssets.ts
+++ src/markets/spark/marketAssets.ts
@@ -1,6 +1,6 @@
 import { NetworkNumber } from '../../types/common';
 
-export const sparkAssetsDefaultMarketEth = ['DAI', 'sDAI', 'USDC', 'ETH', 'wstETH', 'WBTC', 'GNO', 'rETH', 'USDT', 'weETH', 'cbBTC'];
+export const sparkAssetsDefaultMarketEth = ['DAI', 'sDAI', 'USDC', 'ETH', 'wstETH', 'WBTC', 'GNO', 'rETH', 'USDT', 'weETH', 'cbBTC', 'sUSDS'];
 
 // @dev Keep assets in array, do not assign directly, so we can parse it and edit it programmatically with `scripts/updateMarkets`
 export const sparkAssetsDefaultMarket = {
