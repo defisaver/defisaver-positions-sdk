0a37b08c49944f1ff5cbd8070a7191f07710a919
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2025-07-08 16:45:28 +0200

    Add portfolio test

--- src/portfolio/index.ts
+++ src/portfolio/index.ts
@@ -1,7 +1,4 @@
-import Web3 from 'web3';
 import Dec from 'decimal.js';
-import { createPublicClient, custom } from 'viem';
-import { mainnet } from 'viem/chains';
 import { EthAddress, EthereumProvider, NetworkNumber } from '../types/common';
 import {
   AaveMarkets,
@@ -14,7 +11,14 @@ import {
   AaveVersions,
   CdpInfo,
   CompoundV2MarketsData,
-  CompoundV3MarketsData, CompoundVersions, CrvUSDGlobalMarketData, EulerV2FullMarketData, LlamaLendGlobalMarketData, MorphoBlueMarketInfo, PortfolioPositionsData, SparkMarketsData,
+  CompoundV3MarketsData,
+  CompoundVersions,
+  CrvUSDGlobalMarketData,
+  EulerV2FullMarketData,
+  LlamaLendGlobalMarketData,
+  MorphoBlueMarketInfo,
+  PortfolioPositionsData,
+  SparkMarketsData,
 } from '../types';
 import { _getCompoundV3AccountData, _getCompoundV3MarketsData } from '../compoundV3';
 import { _getSparkAccountData, _getSparkMarketsData } from '../spark';
@@ -26,30 +30,29 @@ import { ZERO_ADDRESS } from '../constants';
 import { _getMakerCdpData, _getUserCdps } from '../maker';
 import { _getAaveV2AccountData, _getAaveV2MarketsData } from '../aaveV2';
 import { _getCompoundV2AccountData, _getCompoundV2MarketsData } from '../compoundV2';
+import { getViemProvider } from '../services/viem';
 
 export async function getPortfolioData(provider: EthereumProvider, network: NetworkNumber, defaultProvider: EthereumProvider, addresses: EthAddress[]): Promise<PortfolioPositionsData> {
+  const isMainnet = network === NetworkNumber.Eth;
+
   const morphoMarkets = Object.values(MorphoBlueMarkets(network)).filter((market) => market.chainIds.includes(network));
   const compoundV3Markets = Object.values(CompoundMarkets(network)).filter((market) => market.chainIds.includes(network) && market.value !== CompoundVersions.CompoundV2);
   const sparkMarkets = Object.values(SparkMarkets(network)).filter((market) => market.chainIds.includes(network));
   const eulerV2Markets = Object.values(EulerV2Markets(network)).filter((market) => market.chainIds.includes(network));
   const crvUsdMarkets = Object.values(CrvUsdMarkets(network)).filter((market) => market.chainIds.includes(network));
-  const llamaLendMarkets = Object.values(LlamaLendMarkets(network)).filter((market) => market.chainIds.includes(network));
+  const llamaLendMarkets = [NetworkNumber.Eth, NetworkNumber.Arb].includes(network) ? Object.values(LlamaLendMarkets(network)).filter((market) => market.chainIds.includes(network)) : [];
   const aaveV3Markets = [AaveVersions.AaveV3, AaveVersions.AaveV3Lido, AaveVersions.AaveV3Etherfi].map((version) => AaveMarkets(network)[version]).filter((market) => market.chainIds.includes(network));
   const aaveV2Markets = [AaveVersions.AaveV2].map((version) => AaveMarkets(network)[version]).filter((market) => market.chainIds.includes(network));
   const compoundV2Markets = [CompoundVersions.CompoundV2].map((version) => CompoundMarkets(network)[version]).filter((market) => market.chainIds.includes(network));
 
-  const client = createPublicClient({
-    transport: custom(provider),
-    chain: mainnet,
+  const client = getViemProvider(provider, network, {
     batch: {
       multicall: {
         batchSize: 16384,
       },
     },
   });
-  const defaultClient = createPublicClient({
-    transport: custom(defaultProvider),
-    chain: mainnet,
+  const defaultClient = getViemProvider(defaultProvider, network, {
     batch: {
       multicall: {
         batchSize: 16384,
@@ -94,11 +97,11 @@ export async function getPortfolioData(provider: EthereumProvider, network: Netw
       llamaLendMarketsData[market.value] = marketData;
     }),
     ...aaveV3Markets.map(async (market) => {
-      // @ts-ignore
       const marketData = await _getAaveV3MarketData(client, network, market);
       aaveV3MarketsData[market.value] = marketData;
     }),
     ...addresses.map(async (address) => {
+      if (!isMainnet) return; // Maker CDPs are only available on mainnet
       const makerCdp = await _getUserCdps(client, network, address);
       makerCdps[address.toLowerCase() as EthAddress] = makerCdp;
     }),
@@ -163,7 +166,7 @@ export async function getPortfolioData(provider: EthereumProvider, network: Netw
         positions[address.toLowerCase() as EthAddress].llamaLend[market.value] = accData;
       }
     })).flat(),
-    ...addresses.map(async (address) => makerCdps[address.toLowerCase() as EthAddress].map(async (cdpInfo) => {
+    ...addresses.map(async (address) => makerCdps[address.toLowerCase() as EthAddress]?.map(async (cdpInfo) => {
       const cdpData = await _getMakerCdpData(client, network, cdpInfo);
       if (cdpData) {
         positions[address.toLowerCase() as EthAddress].maker[cdpInfo.id] = cdpData;
