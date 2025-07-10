fab843919c7d1a3481e655c96197084670ed1ebc
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2025-07-10 17:29:37 +0200

    Update portfolio types

--- src/types/portfolio.ts
+++ src/types/portfolio.ts
@@ -11,49 +11,53 @@ import { CdpData } from './maker';
 import { MorphoBluePositionData, MorphoBlueVersions } from './morphoBlue';
 import { SparkPositionData, SparkVersions } from './spark';
 
-export interface PortfolioPositionsData {
-  [key: EthAddress]: {
-    aaveV3: {
-      [key in AaveVersions]?: AaveV3PositionData;
-    };
-    morphoBlue: {
-      [key in MorphoBlueVersions]?: MorphoBluePositionData;
-    };
-    compoundV3: {
-      [key in CompoundVersions]?: CompoundV3PositionData;
-    };
-    spark: {
-      [key in SparkVersions]?: SparkPositionData;
-    }
-    eulerV2: {
-      [key in EulerV2Versions]?: EulerV2PositionData;
-    };
-    maker: {
-      [key: string]: CdpData;
-    };
-    aaveV2: {
-      [key in AaveVersions]?: AaveV2PositionData;
-    };
-    compoundV2: {
-      [key in CompoundVersions]?: CompoundV2PositionData;
-    };
-    liquity: LiquityTroveInfo | {};
+export interface PortfolioPositionsDataForAddress {
+  aaveV3: {
+    [key in AaveVersions]?: AaveV3PositionData;
   };
+  morphoBlue: {
+    [key in MorphoBlueVersions]?: MorphoBluePositionData;
+  };
+  compoundV3: {
+    [key in CompoundVersions]?: CompoundV3PositionData;
+  };
+  spark: {
+    [key in SparkVersions]?: SparkPositionData;
+  }
+  eulerV2: {
+    [key in EulerV2Versions]?: EulerV2PositionData;
+  };
+  maker: {
+    [key: string]: CdpData;
+  };
+  aaveV2: {
+    [key in AaveVersions]?: AaveV2PositionData;
+  };
+  compoundV2: {
+    [key in CompoundVersions]?: CompoundV2PositionData;
+  };
+  liquity: LiquityTroveInfo | {};
+}
+
+export interface PortfolioPositionsData {
+  [key: EthAddress]: PortfolioPositionsDataForAddress;
+}
+
+export interface PortfolioPositionsDataSlowerForAddress {
+  crvUsd: {
+    [key in CrvUSDVersions]?: CrvUSDUserData;
+  };
+  llamaLend: {
+    [key in LlamaLendVersionsType]?: LlamaLendUserData;
+  };
+  liquityV2: {
+    [key in LiquityV2Versions]?: Record<string, LiquityV2TroveData>;
+  };
+  fluid: {
+    [key: string]: FluidVaultData;
+  }
 }
 
 export interface PortfolioPositionsDataSlower {
-  [key: EthAddress]: {
-    crvUsd: {
-      [key in CrvUSDVersions]?: CrvUSDUserData;
-    };
-    llamaLend: {
-      [key in LlamaLendVersionsType]?: LlamaLendUserData;
-    };
-    liquityV2: {
-      [key in LiquityV2Versions]?: Record<string, LiquityV2TroveData>;
-    };
-    fluid: {
-      [key: string]: FluidVaultData;
-    }
-  };
+  [key: EthAddress]: PortfolioPositionsDataSlowerForAddress;
 }
