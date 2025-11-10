ab2d201450c8ab190e23c9c79dd949ca0f80af22
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2025-11-10 17:25:08 +0100

    Optimize morpho vault data fetching

--- src/savings/morphoVaults/index.ts
+++ src/savings/morphoVaults/index.ts
@@ -1,6 +1,7 @@
 import { Client } from 'viem';
+import Dec from 'decimal.js';
 import { request as graphqlRequest } from 'graphql-request';
-import { assetAmountInEth } from '@defisaver/tokens';
+import { assetAmountInEth, getAssetInfo } from '@defisaver/tokens';
 import * as morphoVaultsOptions from './options';
 import { EthAddress, EthereumProvider, NetworkNumber } from '../../types/common';
 import { getViemProvider } from '../../services/viem';
@@ -34,17 +35,20 @@ const MORPHO_BLUE_API = 'https://blue-api.morpho.org/graphql';
 
 export const _getMorphoVaultData = async (provider: Client, network: NetworkNumber, morphoVault: MorphoVault, account: EthAddress): Promise<any> => {
   const morphoVaultContract = getMorphoVaultContractViem(provider, morphoVault.address);
-
-  const [totalAssets, shares, vaultData] = await Promise.all([
+  const [totalAssets, shares, totalSupply, decimals, decimalsOffset, vaultData] = await Promise.all([
     morphoVaultContract.read.totalAssets(),
     morphoVaultContract.read.balanceOf([account]),
+    morphoVaultContract.read.totalSupply(),
+    morphoVaultContract.read.decimals(),
+    morphoVaultContract.read.DECIMALS_OFFSET(),
     graphqlRequest(MORPHO_BLUE_API, vaultDataQuery(morphoVault.address)),
   ]);
 
-  const assets = await morphoVaultContract.read.convertToAssets([shares]);
+  const supplied = new Dec(new Dec(shares.toString()).mul(new Dec(totalAssets.toString()).add(1)).div(new Dec(totalSupply.toString()).add(10 ** decimalsOffset)).div(10 ** 18)
+    .toFixed(decimals)).mul(10 ** decimalsOffset)
+    .toString();
 
   const poolSize = assetAmountInEth(totalAssets.toString(), morphoVault.asset);
-  const supplied = assetAmountInEth(assets.toString(), morphoVault.asset);
   const liquidity = assetAmountInEth((vaultData as any).vaultByAddress.liquidity.underlying, morphoVault.asset);
 
   return {
