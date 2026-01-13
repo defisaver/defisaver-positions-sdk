2909652e17188098ff4d65be2b92954e9e269ec4
Nadja Gvozdenac <nadja.gvozdenac@decenter.com>
2026-01-13 13:03:21 +0100

    DEV-11841: Add PT April to Aave Plasma

--- src/staking/staking.ts
+++ src/staking/staking.ts
@@ -64,7 +64,7 @@ export const STAKING_ASSETS = [
   'cbETH', 'wstETH', 'cbETH', 'rETH', 'sDAI', 'weETH', 'sUSDe', 'osETH',
   'ezETH', 'ETHx', 'rsETH', 'pufETH', 'wrsETH', 'wsuperOETHb', 'sUSDS', 'tETH', 'PT sUSDe Sep', 'PT USDe Sep',
   'PT sUSDe Nov', 'PT USDe Nov', 'PT USDe Jan', 'PT sUSDe Jan', 'wrsETH', 'wstETH', 'syrupUSDT', 'syrupUSDC', 'wstUSR',
-  'PT sUSDe Feb', 'PT USDe Feb',
+  'PT sUSDe Feb', 'PT USDe Feb', 'PT USDe Apr', 'PT sUSDe Apr',
 ];
 
 export const getStakingApy = memoize(async (asset: string, network: number = NetworkNumber.Eth) => {
@@ -99,6 +99,8 @@ export const getStakingApy = memoize(async (asset: string, network: number = Net
     if (asset === 'wstUSR') return await getApyFromDfsApi('wstUSR');
     if (asset === 'PT sUSDe Feb') return await getApyFromDfsApi('PT sUSDe Feb', network);
     if (asset === 'PT USDe Feb') return await getApyFromDfsApi('PT USDe Feb', network);
+    if (asset === 'PT sUSDe Apr') return await getApyFromDfsApi('PT sUSDe Apr', network);
+    if (asset === 'PT USDe Apr') return await getApyFromDfsApi('PT USDe Apr', network);
   } catch (e) {
     console.error(`Failed to fetch APY for ${asset}`);
   }
