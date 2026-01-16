ca48474017fc5aafef9aa2153de5a50e39fd0d95
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2026-01-16 11:29:13 +0100

    Add isLeveragedPosAaveV4

--- src/helpers/aaveV4Helpers/index.ts
+++ src/helpers/aaveV4Helpers/index.ts
@@ -1,11 +1,63 @@
 import Dec from 'decimal.js';
-import { calcLeverageLiqPrice, getAssetsTotal, isLeveragedPos } from '../../moneymarket';
+import { calcLeverageLiqPrice, getAssetsTotal, STABLE_ASSETS } from '../../moneymarket';
 import {
   AaveV4AggregatedPositionData, AaveV4AssetsData, AaveV4ReserveAssetData, AaveV4UsedReserveAsset, AaveV4UsedReserveAssets,
 } from '../../types';
 import { NetworkNumber } from '../../types/common';
 
 export const aaveV4GetCollateralFactor = (assetData: AaveV4ReserveAssetData, usedAssetData: AaveV4UsedReserveAsset, useUserCollateralFactor: boolean = false): number => (useUserCollateralFactor ? usedAssetData.collateralFactor : assetData.collateralFactor);
+
+export const isLeveragedPosAaveV4 = (usedAssets: AaveV4UsedReserveAssets, dustLimit = 5) => {
+  let borrowUnstable = 0;
+  let supplyStable = 0;
+  let borrowStable = 0;
+  let supplyUnstable = 0;
+  let longAsset = '';
+  let shortAsset = '';
+  Object.values(usedAssets).forEach(({
+    symbol, suppliedUsd, borrowedUsd, collateral, reserveId,
+  }) => {
+    const spokeAsset = `${symbol}-${reserveId}`;
+    const isSupplied = (+suppliedUsd) > dustLimit; // ignore dust like <limit leftover supply
+    const isBorrowed = (+borrowedUsd) > dustLimit; // ignore dust like <limit leftover supply
+    if (isSupplied && STABLE_ASSETS.includes(symbol) && collateral) supplyStable += 1;
+    if (isBorrowed && STABLE_ASSETS.includes(symbol)) borrowStable += 1;
+    if (isBorrowed && !STABLE_ASSETS.includes(symbol)) {
+      borrowUnstable += 1;
+      shortAsset = spokeAsset;
+    }
+    if (isSupplied && !STABLE_ASSETS.includes(symbol) && collateral) {
+      supplyUnstable += 1;
+      longAsset = spokeAsset;
+    }
+  });
+  const isLong = borrowStable > 0 && borrowUnstable === 0 && supplyUnstable === 1 && supplyStable === 0;
+  const isShort = supplyStable > 0 && supplyUnstable === 0 && borrowUnstable === 1 && borrowStable === 0;
+  // lsd -> liquid staking derivative
+  const isLsdLeveraged = supplyUnstable === 1 && borrowUnstable === 1 && shortAsset === 'ETH' && ['stETH', 'wstETH', 'cbETH', 'rETH', 'ezETH', 'weETH'].includes(longAsset);
+  if (isLong) {
+    return {
+      leveragedType: 'long',
+      leveragedAsset: longAsset,
+    };
+  }
+  if (isShort) {
+    return {
+      leveragedType: 'short',
+      leveragedAsset: shortAsset,
+    };
+  }
+  if (isLsdLeveraged) {
+    return {
+      leveragedType: 'lsd-leverage',
+      leveragedAsset: longAsset,
+    };
+  }
+  return {
+    leveragedType: '',
+    leveragedAsset: '',
+  };
+};
 
 export const aaveV4GetAggregatedPositionData = ({
   usedAssets,
@@ -36,7 +88,7 @@ export const aaveV4GetAggregatedPositionData = ({
   payload.collRatio = +payload.suppliedUsd ? new Dec(payload.suppliedCollateralUsd).div(payload.borrowedUsd).mul(100).toString() : '0';
   payload.liqRatio = new Dec(payload.borrowLimitUsd).div(payload.liquidationLimitUsd).toString();
   payload.liqPercent = new Dec(payload.borrowLimitUsd).div(payload.liquidationLimitUsd).mul(100).toString();
-  const { leveragedType, leveragedAsset } = isLeveragedPos(usedAssets);
+  const { leveragedType, leveragedAsset } = isLeveragedPosAaveV4(usedAssets);
   payload.leveragedType = leveragedType;
   payload.leveragedAsset = leveragedAsset;
   payload.liquidationPrice = '';
